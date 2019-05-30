60316a0707acaef0100ca272ef4d6e9aeb1fd999
ChaoLiou <ucnsaythtagn@gmail.com>
2019-05-30 16:03:32 +0800

    update

--- server/src/export/annual/index.js
+++ server/src/export/annual/index.js
@@ -17,6 +17,7 @@ function populate(year, data, cb) {
                 const column = sheet.column(header.name)
                 const cell = initCell(sheet, header.name)
                 initStyles(header, cell, column, sheet, header.merged)
+                printUnderline(cell)
                 const year1 = year + header.dataKey.index - 1
                 const year2 = year + header.dataKey.index
                 const title = isStringValue(header.title) ?
@@ -24,67 +25,70 @@ function populate(year, data, cb) {
                     stringFormat(header.title.format, [`${year1}-${year2}`])
                 cell.value(title)
             })
-            data.filter(employee => employee.enabled)
-                .forEach((employee, index) => {
-                    const rowNumber = index + 1 + 1
-                    headers.forEach(header => {
-                        const cell = initCell(sheet, header.name, rowNumber)
-                        initStyles(header, cell)
-                        if (isEmployeeInfoArea(header.dataKey)) {
-                            cell.value(employee[header.dataKey])
-                        } else if (isNotEmployeeInfoArea(header.dataKey)) {
-                            if (isMonthlyReportArea(header.dataKey.month)) {
-                                const target = employee.recordGroups.find(group => group.month === header.dataKey.month)
-                                cell.value(new RichText())
-                                const richText = cell.value()
-                                target.list.forEach(item => {
-                                    const targetHeader = headers.find(header => header.dataKey.leaveType === item.key)
-                                    const targetStyle = targetHeader ? targetHeader.styles.find(style => style.name === 'fontColor') : undefined
-                                    const fontColor = targetStyle ? targetStyle.value : undefined
-                                    richText.add(`${totalsToString(item.totals, item.key)}\n`, { fontColor })
-                                })
-                            } else if (isLTReportArea(header.dataKey.leaveType)) {
-                                const compensatoryLTName = "compensatory"
-                                if (header.dataKey.leaveType === compensatoryLTName) {
-                                    const target = employee.leaveTypeGroups
-                                        .find(group => header.dataKey.leaveType === compensatoryLTName && isCompensatoryLT(group.key))
-                                    if (target) {
+            const enabledEmployees = data.report.filter(employee => employee.enabled)
+            enabledEmployees.forEach((employee, index) => {
+                const rowNumber = index + 1 + 1
+                headers.forEach(header => {
+                    const cell = initCell(sheet, header.name, rowNumber)
+                    initStyles(header, cell)
+                    if (isLastRow(enabledEmployees.length, index)) {
+                        printUnderline(cell)
+                    }
+                    if (isEmployeeInfoArea(header.dataKey)) {
+                        cell.value(employee[header.dataKey])
+                    } else if (isNotEmployeeInfoArea(header.dataKey)) {
+                        if (isMonthlyReportArea(header.dataKey.month)) {
+                            const target = employee.recordGroups.find(group => group.month === header.dataKey.month)
+                            cell.value(new RichText())
+                            const richText = cell.value()
+                            target.list.sort(comparer).forEach(item => {
+                                const targetHeader = headers.find(header => header.dataKey.leaveType === item.key)
+                                const targetStyle = targetHeader ? targetHeader.styles.find(style => style.name === 'fontColor') : undefined
+                                const fontColor = targetStyle ? targetStyle.value : undefined
+                                richText.add(`${totalsToString(item.totals, item.key)}\n`, { fontColor })
+                            })
+                        } else if (isLTReportArea(header.dataKey.leaveType)) {
+                            const compensatoryLTName = "compensatory"
+                            if (header.dataKey.leaveType === compensatoryLTName) {
+                                const target = employee.leaveTypeGroups
+                                    .find(group => header.dataKey.leaveType === compensatoryLTName && isCompensatoryLT(group.key))
+                                if (target) {
+                                    const totals = totalsListMapping(target.list, header.dataKey.unit);
+                                    sumUpLTTotals(allLTsTotalsCounters, totals, compensatoryLTName, header.dataKey.unit)
+                                    cell.value(totalsToString(totals))
+                                }
+                            } else {
+                                const target = employee.leaveTypeGroups
+                                    .find(group => group.key === header.dataKey.leaveType)
+                                if (target) {
+                                    if (header.dataKey.unit) {
                                         const totals = totalsListMapping(target.list, header.dataKey.unit);
-                                        sumUpLTTotals(allLTsTotalsCounters, totals, compensatoryLTName, header.dataKey.unit)
+                                        sumUpLTTotals(allLTsTotalsCounters, totals, target.key, header.dataKey.unit)
+                                        cell.value(totalsToString(totals))
+                                    } else {
+                                        const totals = sumUpTotals(target.list.map(item => item.totals))
+                                        sumUpLTTotals(allLTsTotalsCounters, totals, target.key)
                                         cell.value(totalsToString(totals))
                                     }
-                                } else {
-                                    const target = employee.leaveTypeGroups
-                                        .find(group => group.key === header.dataKey.leaveType)
-                                    if (target) {
-                                        if (header.dataKey.unit) {
-                                            const totals = totalsListMapping(target.list, header.dataKey.unit);
-                                            sumUpLTTotals(allLTsTotalsCounters, totals, target.key, header.dataKey.unit)
-                                            cell.value(totalsToString(totals))
-                                        } else {
-                                            const totals = sumUpTotals(target.list.map(item => item.totals))
-                                            sumUpLTTotals(allLTsTotalsCounters, totals, target.key)
-                                            cell.value(totalsToString(totals))
-                                        }
-                                    }
-                                }
-                            } else if (isAnnualInfoArea(header.dataKey.index)) {
-                                const info = employee.annualInfo[header.dataKey.index]
-                                if (header.dataKey.type === "remaining") {
-                                    const remainingTotals = { days: info.totalDays - info.totals.days }
-                                    const remainingStr = totalsToString(remainingTotals)
-                                    cell.value(remainingStr ? remainingStr : '0天')
-                                } else if (header.dataKey.type === "deadline") {
-                                    const deadlinePlusXYear = (year + header.dataKey.index) + employee.arrivedDate.substr(4)
-                                    cell.value(deadlinePlusXYear)
                                 }
                             }
+                        } else if (isAnnualInfoArea(header.dataKey.index)) {
+                            const info = employee.annualInfo[header.dataKey.index]
+                            if (header.dataKey.type === "remaining") {
+                                const remainingTotals = { days: info.totalDays - info.totals.days }
+                                const remainingStr = totalsToString(remainingTotals)
+                                cell.value(remainingStr ? remainingStr : '0天')
+                            } else if (header.dataKey.type === "deadline") {
+                                const deadlinePlusXYear = (year + header.dataKey.index) + employee.arrivedDate.substr(4)
+                                cell.value(deadlinePlusXYear)
+                            }
                         }
-                    })
+                    }
                 })
+            })
             headers.filter(header => isNotEmployeeInfoArea(header.dataKey) && isLTReportArea(header.dataKey.leaveType))
                 .forEach(header => {
-                    const cell = sheet.cell(`${header.name}${data.length + 3}`)
+                    const cell = sheet.cell(`${header.name}${data.report.length + 2}`)
                     header.styles.forEach(style => cell.style(style.name, style.value))
                     const key = header.dataKey.unit ? `${header.dataKey.leaveType}.${header.dataKey.unit}` : header.dataKey.leaveType
                     cell.value(totalsToString(sumUpTotals(allLTsTotalsCounters[key])))
@@ -203,4 +207,31 @@ function isLTReportArea(leaveType) {
 
 function isAnnualInfoArea(headerDataKeyIndex) {
     return isNumberValue(headerDataKeyIndex)
+}
+
+function isLastRow(length, index) {
+    return index === length - 1
+}
+
+function printUnderline(cell) {
+    cell.style("bottomBorderStyle", "medium")
+}
+
+function comparer(a, b) {
+    const aScore = getLTScore(a.key)
+    const bScore = getLTScore(b.key)
+    return aScore < bScore
+}
+
+function getLTScore(leaveType) {
+    const scores = {
+        "personal": 0,
+        "sick": 1,
+        "menstrual": 2,
+        "funeral": 3,
+        "marriage": 4,
+        "compensatory": 5,
+        "annual": 6
+    }
+    return scores[leaveType] ? scores[leaveType] : (isCompensatoryLT(leaveType) ? scores["compensatory"] : -1)
 }
